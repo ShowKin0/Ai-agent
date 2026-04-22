9e0b35dee0e96518249a409451cf8304ecdff03c
ShowKin <3064219762@qq.com>
2026-04-22 17:21:56 +0800

    add

--- LangChain.py
+++ LangChain.py
@@ -1,11 +1,11 @@
-from langchain_community.llms.tongyi import Tongyi
+from langchain.chat_models import init_chat_model
 from langchain_core.prompts import PromptTemplate
 import os
 from dotenv import load_dotenv
 load_dotenv()
 model_key=os.getenv("Tongyi_API_KEY")
 model_id=os.getenv("Tongyi_MODEL_ID")
-Model=Tongyi(api_key=model_key, model_id=model_id)
+Model=init_chat_model(api_key=model_key, model_id=model_id)
 promptz_template=PromptTemplate.from_template(
     "你好,你是一个专业股票手,帮我分析一下这只股票{stock}"
 )
