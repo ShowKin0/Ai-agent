8b296971cb9578903a0bf5ceb20c5c6dd9336a08
ShowKin <3064219762@qq.com>
2026-04-22 17:36:15 +0800

    add

--- LangChain.py
+++ LangChain.py
@@ -1,16 +1,17 @@
-from langchain_community.llms.tongyi import Tongyi
+from langchain.chat_models import init_chat_model
 from langchain_core.prompts import PromptTemplate
 import os
 from dotenv import load_dotenv
 load_dotenv()
 model_key=os.getenv("Tongyi_API_KEY")
 model_id=os.getenv("Tongyi_MODEL_ID")
-Model=Tongyi(api_key=model_key, model_id=model_id)
+base_url=os.getenv("Tongyi_BASE_URL")
+Model=init_chat_model(api_key=model_key, model=model_id,model_provider="openai",base_url=base_url)
 promptz_template=PromptTemplate.from_template(
     "你好,你是一个专业股票手,帮我分析一下这只股票{stock}"
 )
 promptz=promptz_template.format(stock="513180")
 response=Model.stream(promptz)
 for chunk in response:
-    print(chunk, end="", flush=True)
+    print(chunk.content, end="", flush=True)
     
